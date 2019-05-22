0a37027896d4114dbf10b951de8cc2eeaa00bfea
Colin Smith <ColinSmith1@boisestate.edu>
2019-05-22 17:50:01 -0600

    fix formatting for father linter

--- client/src/components/RSVPForm.vue
+++ client/src/components/RSVPForm.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>    
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
-<script>export default {
+<script>
+export default {
   name: 'RSVPForm',
   props: {
     msg: String
   }
-}</script>
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
