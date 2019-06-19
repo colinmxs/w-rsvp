51d8dae26149081751bae9ec5bd2ba290cc58d77
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-18 23:08:48 -0600

    add router and some preliminary routes

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from './components/Login'
+import Home from './components/Home'
 import App from './App.vue'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+// Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '', component: Home },
+  { path: '/login', component: Login }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
