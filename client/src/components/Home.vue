648f8afb72e89ba611516bd6e6d45a06c6f1851c
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-18 23:05:12 -0600

    add Home.vue

---
+++ client/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <h1>W-RSVP!</h1>
+    <p>Free Party/Wedding RSVP Form with QR Codes!</p>
+    <router-link to="/login">Login or Register!</router-link>
+    <!--<a href="https://github.com/colinmxs/w-rsvp/issues">Github</a>-->
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
