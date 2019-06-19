66394c95179633c1a7b28670b6b356335e0f5787
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-18 23:09:38 -0600

    update App.vue w/font css and router-view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <RSVPForm msg=""/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import RSVPForm from './components/RSVPForm.vue'
 
 export default {
-  name: 'app',
-  components: {
-    RSVPForm
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  h1 {
+    font-family: 'Lobster', cursive;
+  }
+  p {
+    font-family: 'Roboto', sans-serif;
+  }
 </style>
