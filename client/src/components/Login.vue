118b4e542af7cf2f4b8b5f23a34cbdeb8d6d2a33
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-18 23:06:44 -0600

    add/implement Login.vue

---
+++ client/src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="login">    
+    <g-signin-button :params="googleSignInParams"
+                     @success="onSignInSuccess"
+                     @error="onSignInError">
+      Sign in with Google
+    </g-signin-button>
+  </div>
+</template>
+
+<script>
+  import GoogleSignInButton from './GoogleSignInButton'
+  import { authenticationService } from '../_services/authentication.service'
+export default {
+  data () {
+    return {
+      /**
+       * The Auth2 parameters, as seen on
+       * https://developers.google.com/identity/sign-in/web/reference#gapiauth2initparams.
+       * As the very least, a valid client_id must present.
+       * @type {Object}
+       */
+      googleSignInParams: {
+        client_id: '1056158958261-u9o1s4i9srb3tr6ksqk1jk3o17h358pc.apps.googleusercontent.com'
+      }
+    }
+    },
+    components: {
+      'g-signin-button' : GoogleSignInButton
+    },
+  methods: {
+    onSignInSuccess (googleUser) {
+      authenticationService.login(googleUser)
+      this.$router.push({ name: 'user', params: { userId: '123' } })
+    },
+    onSignInError (error) {
+      // `error` contains any error occurred.
+      console.log('OH NOES', error)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
