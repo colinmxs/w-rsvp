1d9a5f5db1310ecc9543b52f7803f2ef5c00d5c6
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-19 22:55:44 -0600

    move accountcontroller into feature folder and implement Post action

---
+++ server/src/Features/Account/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using w_rsvp.Api.Features.Account.Commands;
+
+namespace w_rsvp.Api.Features.Account
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<CreateAccount.Result> Post([FromBody]CreateAccount.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
