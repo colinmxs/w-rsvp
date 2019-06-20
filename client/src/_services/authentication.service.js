454a124c6e82389f8e39c68637fb27235aa0c3b6
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-19 22:57:05 -0600

    call local api/account/create service during login

--- client/src/_services/authentication.service.js
+++ client/src/_services/authentication.service.js
@@ -1,12 +1,24 @@
+import axios from 'axios'
+
 export const authenticationService = {
   login,
   logout,
   get currentUser () { return JSON.parse(localStorage.getItem('currentUser')) }
 }
 
-function login (googleUser) {
-  const profile = googleUser.getBasicProfile() // etc etc
+async function login (googleUser) {
+  const profile = googleUser.getBasicProfile()
   const idToken = googleUser.getAuthResponse().id_token
+
+  try{
+    const response = await axios.post('http://localhost:56215/api/account', {
+        idToken: idToken
+    })
+    console.log(response);
+  } catch (e) {
+    //handle
+  }
+
   localStorage.setItem('currentUser', JSON.stringify(profile))
   localStorage.setItem('id_token', JSON.stringify(idToken))
 }
