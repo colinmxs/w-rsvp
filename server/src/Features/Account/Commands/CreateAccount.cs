4f965b6f2aecffb1a98f05e2fc088fbc70411dab
Colin Smith <ColinSmith1@boisestate.edu>
2019-06-19 22:54:57 -0600

    add CreateAccount.Command/CommandHandler

---
+++ server/src/Features/Account/Commands/CreateAccount.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.DynamoDBv2;
+using Google.Apis.Auth;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace w_rsvp.Api.Features.Account.Commands
+{
+    public class CreateAccount
+    {
+        public class Command : IRequest<Result>
+        {
+            public string IdToken { get; set; }
+        }
+
+        public class Result
+        {
+
+        }
+
+        public class CommandHandler : IRequestHandler<Command, Result>
+        {
+            private IAmazonDynamoDB _dbClient;
+
+            public CommandHandler(IAmazonDynamoDB dbClient)
+            {
+                _dbClient = dbClient;
+            }
+
+            public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken);
+
+                throw new System.NotImplementedException();
+
+            }
+        }
+    }
+}
