46b61a7de746a20b442e3032fa9e7de29a9c7b78
Colin Smith <ColinSmith1@boisestate.edu>
2019-07-02 22:30:19 -0600

    add apiService and call getUserData

--- client/src/components/User.vue
+++ client/src/components/User.vue
@@ -1,13 +1,30 @@
 <template>
-
+  <div>{{userId}}</div>
 </template>
 
 <script>
+import { apiService } from '../_services/api.service' 
   export default {
     name: 'user',
     data() {
       return {
-
+        userId: "unknown"
+      }
+    },
+    async created () {
+      console.log('User component created')
+      await this.getUserData(this.$route.params.userId)
+    },
+    async beforeRouteUpdate (to, from, next) {
+      await this.getUserData(to.params.userId)
+      next()
+    },
+    methods: {
+      async getUserData (userId) {
+        console.log('getUserData(' + userId + ')')
+        const userData = await apiService.getUserData(userId, )
+        console.log(userData)
+        this.userId = userId
       }
     }
   }
